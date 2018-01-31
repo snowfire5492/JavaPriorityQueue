f124c1af4a861511660bc5d9d39db21e9960c241
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2018-01-31 12:12:12 -0800

    update

--- Node.java
+++ Node.java
@@ -1,1 +1,35 @@
 
+
+public class Node implements Comparable<Node>{
+		
+		String name;
+		int value;
+		
+		public Node(String name, int value) {
+			this.name = name;
+			this.value = value;
+		}
+		
+		public int getValue() {
+			return value;
+		}
+		
+		public String toString() {
+			String str = " ";
+			str = "Name: " + name + ", value : " + value;
+			return str;
+		}
+
+		@Override
+		public int compareTo(Node state) {
+			if(this.getValue() > state.getValue()) {
+				return 1;
+			} else if (this.getValue() < state.getValue()) {
+				return -1;
+			} else {
+				return 0;
+			}
+		}
+		
+	}
+	
