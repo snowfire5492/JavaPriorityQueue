5efd694c40620e01f60ec54e17a9eb153317d1e5
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2018-01-31 12:12:12 -0800

    update code

--- HowToUse.java
+++ HowToUse.java
@@ -1,1 +1,31 @@
 
+import java.util.PriorityQueue;
+
+
+public class HowToUse {
+
+	public static void main(String[] args) {
+		
+		PriorityQueue<Node> testQueue = new PriorityQueue<>();	
+		
+		
+		testQueue.add(new Node("A", 1));
+
+		testQueue.add(new Node("B", 2));
+
+		testQueue.add(new Node("C", 3));
+		testQueue.add(new Node("D", 4));
+		
+		
+		
+		 while (!testQueue.isEmpty()) {
+	            System.out.println(testQueue.remove());
+	        }
+	
+	
+	} // end of main
+	
+
+}
+
+
